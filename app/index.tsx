61b683ca130955793e9686489c136f164444e7da
trieuquocbui <buiquoctrieu020402@gmail.com>
2024-07-19 21:09:30 +0700

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+const  IndexScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Hello React native! 🎉</Text>
+    </View>
+  );
+}
+
+export default IndexScreen
+
